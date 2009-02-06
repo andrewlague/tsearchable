c4f161826c6b8039d8def0b604d33af03a80d542
Andrew Lague <andrew.lague@cbst.ucdavis.edu>
2009-02-06 14:30:57 -0800

    no need for results class because we are using ActiveRecord directly through a named_scope; no need to modify postgresql ActiveRecord valid columns so postgresql_extensions is no longer needed

--- test/schema.rb
+++ test/schema.rb
@@ -2,7 +2,6 @@ ActiveRecord::Schema.define(:version => 0) do
   create_table :articles, :force => true do |t|
     t.string     :title
     t.text       :body
-    t.tsvector   :vectors
     t.timestamps
   end
 end
