24aa5b5ce81afbced0c384c8defe3b99d82e3b1a
Andrew Lague <andrew.lague@cbst.ucdavis.edu>
2009-02-06 17:50:37 -0800

    no longer load entire rails environment for tests, just activerecord

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
-ENV['RAILS_ENV'] = 'test'
-ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/../../../..'
-
-require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment.rb'))
+require 'rubygems'
+require 'activerecord'
+require File.dirname(__FILE__) + '/../init'
 
 config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/debug.log')
